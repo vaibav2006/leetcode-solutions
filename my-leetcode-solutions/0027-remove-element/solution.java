e8a9d18d052c17df2e623a1e0a3a82a938772a0c
vaibav2006 <vaibavmugesh_bit27@mepcoeng.ac.in>
2026-02-01 14:22:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.8 MB (18.23%)

---
+++ my-leetcode-solutions/0027-remove-element/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int len=nums.length,count=0;
+        for(int i=0;i<len;i++){
+            if(nums[i]!=val){
+                nums[count]=nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
