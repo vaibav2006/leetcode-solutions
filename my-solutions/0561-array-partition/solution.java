4f956c41804b35eb44481f5ed8e94db665d423a2
vaibav2006 <vaibavmugesh_bit27@mepcoeng.ac.in>
2026-02-08 04:20:50 +0000

    [LeetCode Sync] Runtime - 17 ms (81.41%), Memory - 49.7 MB (36.43%)

---
+++ my-solutions/0561-array-partition/solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        int i = 0;
+        while (i < nums.length) {
+            sum += nums[i];
+            i += 2;
+        }
+        return sum;
+    }
+}
