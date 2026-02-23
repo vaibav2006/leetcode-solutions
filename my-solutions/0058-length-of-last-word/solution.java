519ce5f0bd0cdb7787572b20d91863f238e9abc6
vaibav2006 <vaibavmugesh_bit27@mepcoeng.ac.in>
2026-02-23 13:55:17 +0000

    [LeetCode Sync] Runtime - 2 ms (10.37%), Memory - 43.4 MB (6.44%)

--- my-solutions/0058-length-of-last-word/solution.java
+++ my-solutions/0058-length-of-last-word/solution.java
@@ -1,16 +1,7 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int end = s.length() - 1;
-
-        while (end >= 0 && s.charAt(end) == ' ') {
-            end--;
-        }
-
-        int start = end;
-        while (start >= 0 && s.charAt(start) != ' ') {
-            start--;
-        }
-
-        return end - start;        
+        int k=0;
+        String[] t = s.split("[ ,!]+");
+        return t[t.length-1].length();
     }
 }
