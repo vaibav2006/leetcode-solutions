945bdec6f1900e055cdb80efd715574e15e4cdb7
vaibav2006 <vaibavmugesh_bit27@mepcoeng.ac.in>
2026-02-23 13:56:04 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45 MB (12.46%)

--- my-solutions/0035-search-insert-position/solution.java
+++ my-solutions/0035-search-insert-position/solution.java
@@ -1,15 +1,10 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length-1;
-
-        while (start <= end) {
-            int mid = start + (end-start)/2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) end = mid-1;
-            else start = mid+1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>=target){
+                return i;
+            }
         }
-
-        return start;
+        return nums.length;
     }
 }
