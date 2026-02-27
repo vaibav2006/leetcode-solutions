df2117ff7c97b4696e761303eaf66ab355f7252f
vaibav2006 <vaibavmugesh_bit27@mepcoeng.ac.in>
2026-02-27 15:16:37 +0000

    [LeetCode Sync] Runtime - 3 ms (95.37%), Memory - 42.3 MB (96.02%)

---
+++ my-solutions/2752-sum-multiples/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int sumOfMultiples(int n) {
+        int count=0,sum=0;
+        for(int i=1;i<=n;i++){
+            if(i%3==0 || i%5==0 || i%7==0){
+                sum+=i;
+                count++;
+            }
+        }
+
+        return sum;
+    }
+}
