a8ec8c6e37ae8c818a7d1723233e266ed4c89ac5
vaibav2006 <vaibavmugesh_bit27@mepcoeng.ac.in>
2026-03-06 07:03:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 44.3 MB (88.21%)

---
+++ my-solutions/1791-richest-customer-wealth/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+         
+        int n=accounts.length;
+        int m=accounts[0].length;
+        int f=0;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=0;j<m;j++){
+                sum+=accounts[i][j];
+                if(f<sum){
+                    f=sum;
+                }
+                
+            }
+        }
+        return f;
+    }
+}
