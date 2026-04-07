736ff12e1094a069d98169194af40d63dad3cf33
vaibav2006 <vaibavmugesh_bit27@mepcoeng.ac.in>
2026-04-07 06:22:19 +0000

    [LeetCode Sync] Runtime - 3 ms (93.85%), Memory - 46.7 MB (16.63%)

---
+++ my-solutions/0242-valid-anagram/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] charArray1 = s.toCharArray();
+        char[] charArray2 = t.toCharArray();
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+        return Arrays.equals(charArray1, charArray2);
+
+    }
+}
