a2f56880f7c0f3bd3efc1160422e57229392ea57
vaibav2006 <vaibavmugesh_bit27@mepcoeng.ac.in>
2026-04-23 06:46:14 +0000

    [LeetCode Sync] Runtime - 6 ms (38.01%), Memory - 44.9 MB (57.84%)

---
+++ my-solutions/0414-third-maximum-number/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        int count = 0;
+        Arrays.sort(nums);
+        for(int i = nums.length - 1; i > 0; i--){
+            if(nums[i] != nums[i - 1]) count++;
+            if(count == 2) return nums[i - 1];
+        }
+        return nums[nums.length - 1];
+    }
+}
