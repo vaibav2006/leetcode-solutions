1903b5eec1ade067e2eba6583e92316418b352a2
vaibav2006 <vaibavmugesh_bit27@mepcoeng.ac.in>
2026-06-19 10:46:32 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 46.8 MB (90.83%)

---
+++ my-solutions/2614-maximum-count-of-positive-integer-and-negative-integer/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumCount(int[] nums) {
+        int p=0,n=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                p++;
+            }
+            if(nums[i]<0){
+                n++;
+            }
+        }
+        return Math.max(p,n);
+    }
+}
