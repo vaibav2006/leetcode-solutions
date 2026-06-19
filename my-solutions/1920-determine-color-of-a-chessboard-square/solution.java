a9e9a4b57f686247dda0b508f7f0a87b3354141c
vaibav2006 <vaibavmugesh_bit27@mepcoeng.ac.in>
2026-06-19 06:51:37 +0000

    [LeetCode Sync] Runtime - 1 ms (1.11%), Memory - 42.6 MB (73.60%)

---
+++ my-solutions/1920-determine-color-of-a-chessboard-square/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+          String[][] mat = {
+    {"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"},
+    {"a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"},
+    {"a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"},
+    {"a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"},
+    {"a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"},
+    {"a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"},
+    {"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"},
+    {"a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"}
+};
+        String[][] chessColors = {
+    {"W", "B", "W", "B", "W", "B", "W", "B"}, 
+    {"B", "W", "B", "W", "B", "W", "B", "W"}, 
+    {"W", "B", "W", "B", "W", "B", "W", "B"}, 
+    {"B", "W", "B", "W", "B", "W", "B", "W"}, 
+    {"W", "B", "W", "B", "W", "B", "W", "B"}, 
+    {"B", "W", "B", "W", "B", "W", "B", "W"}, 
+    {"W", "B", "W", "B", "W", "B", "W", "B"}, 
+    {"B", "W", "B", "W", "B", "W", "B", "W"}  
+};
+        String s="";
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat.length;j++){
+                if(mat[i][j].equals(coordinates)){
+                    s+=chessColors[i][j];
+                }
+            }
+        }
+        if(s.equals("W")){
+            return true;
+        }
+        else return false;
+    }
+}
