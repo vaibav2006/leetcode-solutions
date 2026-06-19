62c7e3e6fe40d021e084fc895063e1180061e976
vaibav2006 <vaibavmugesh_bit27@mepcoeng.ac.in>
2026-06-19 11:07:35 +0000

    [LeetCode Sync] Runtime - 15 ms (59.89%), Memory - 51.3 MB (75.01%)

---
+++ my-solutions/0455-assign-cookies/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0, j = 0, count = 0;
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                count++;
+                i++;
+                j++;
+            } else if (g[i] > s[j]) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return count;
+    }
+}
