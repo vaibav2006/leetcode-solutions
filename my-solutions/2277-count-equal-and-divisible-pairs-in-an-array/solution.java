4b98dbb0bec0a2bb43837c3b8ae48667f7630a72
vaibav2006 <vaibavmugesh_bit27@mepcoeng.ac.in>
2026-08-03 10:24:39 +0000

    [LeetCode Sync] Runtime - 3 ms (100.00%), Memory - 44.1 MB (93.64%)

---
+++ my-solutions/2277-count-equal-and-divisible-pairs-in-an-array/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countPairs(int[] nums, int k) {
+        int c=0;
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                    int a=i*j;
+                    if(a%k==0){
+                        c++;
+                    }
+                }
+            }
+        }
+        return c;
+    }
+}
